f93f11e0373297c0b4098670c32fadb5501b74fb
judgesupreme <1234surajsinghnegi@gmail.com>
2020-08-12 16:10:36 +0530

    update nd delete

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ function App() {
 
   useEffect(()=>{
         db.collection('todos').orderBy('timestamp','desc').onSnapshot((snapshot)=>{
-            setTodos(snapshot.docs.map(doc=>doc.data().todo));
+            setTodos(snapshot.docs.map(doc=>({
+              id:doc.id,
+              todo:doc.data().todo
+            })));
         });
   },[]);
 
@@ -48,7 +51,7 @@ function App() {
       <ul>
        {
          todos.map((todo)=>(
-              <Todo text={todo}/>
+              <Todo todo={todo}/>
          ))
        }
      </ul>
--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./Todo.css";
-import { ListItem, List, ListItemText, Avatar, ListItemAvatar } from '@material-ui/core';
+import { ListItem, List, ListItemText, Avatar, ListItemAvatar, Button } from '@material-ui/core';
+import { db } from './firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import firebase from "firebase";
+
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50 ;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 function Todo(props) {
+
+
+    const[open, setOpen]= useState(false);
+    const [input, setInput]= useState('');
+
     const dateBuilder=(d)=>{
         let months=["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
         let date=d.getDate();
@@ -11,15 +44,43 @@ function Todo(props) {
     
         return ` ${date} ${month} ${year}`
       }
+
+      const classes = useStyles();
+      const [modalStyle] = useState(getModalStyle);
+      const updateTodo=()=>{
+                db.collection('todos').doc(props.todo.id).set({
+                    todo:input,
+                    timestamp:firebase.firestore.FieldValue.serverTimestamp()
+                },{ merge:true})
+                setOpen(false);
+      }
+
     return (
+        <>
+         <Modal
+        open={open}
+        onClose={()=>setOpen(false)}>
+            <div style={modalStyle} className={classes.paper}>
+                <form>
+                    <h1>Update The Todo</h1>
+                    <input placeholder={props.todo.todo} value={input} 
+                    onChange={(e)=>setInput(e.target.value)}/>
+                    <Button type="submit" onClick={updateTodo} variant="contained" color="primary" disabled={!input}>
+                        Update Todo</Button>
+                </form>
+            </div>
+        </Modal>
         <List className="todo__list">
             <ListItem>
                 <ListItemAvatar>
                 </ListItemAvatar>
-                <ListItemText primary={props.text} secondary={dateBuilder(new Date())}/>
+                <ListItemText primary={props.todo.todo} secondary={dateBuilder(new Date())}/>
             </ListItem>
+            <button onClick={()=>setOpen(true)}>Edit Me</button>
+            <DeleteForeverIcon onClick={e=>db.collection('todos').doc(props.todo.id).delete()}/>
         </List>
+        </>
     )
 }
 
-export default Todo
+export default Todo;
